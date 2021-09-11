d548d06453cf8aed839dbc214192f62e5315ee4f
Harvey Hase <harvey186@hotmail.com>
2021-09-11 11:30:52 +0200

    10_about_changes

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 #LeOS Launcher
 This is a fork of Omega Launcher and default Launcher in LeOS-GSI. 
+Please visit https://leos-gsi.de
 Releases are stored at https://leos-gsi.de/downloads/special_apps/Launcher/
--- build/generated/source/buildConfig/aospWithQuickstepOmega/debug/com/android/launcher3/BuildConfig.java
+++ build/generated/source/buildConfig/aospWithQuickstepOmega/debug/com/android/launcher3/BuildConfig.java
@@ -14,7 +14,7 @@ public final class BuildConfig {
   public static final int VERSION_CODE = 2;
   public static final String VERSION_NAME = "1";
   // Field from default config.
-  public static final String BUILD_DATE = "2021-09-11T07:54Z";
+  public static final String BUILD_DATE = "2021-09-11T09:26Z";
   // Field from default config.
   public static final String[] DETECTED_ANDROID_LOCALES = {"de","hi","lo","pt","lt","hr","lv","hu","en-rIN","hy","uk","ur","mk","ml","mn","uz","af","in","fr-rCA","mr","ms","el","zh-rHK","zh-rTW","en","is","it","am","my","iw","es","b+sr+Latn","et","eu","ar","as","vi","nb","es-rUS","ja","ne","az","pt-rPT","rm","zu","fa","ro","nl","zh-rCN","ru","be","fi","en-rXC","bg","fr","bn","bs","en-rCA","en-rGB","ka","si","sk","sl","ca","sq","sr","kk","km","kn","or","sv","sw","ko","gl","ta","ky","gu","cs","pa","te","th","tl","pl","da","tr","en-rAU"};
   // Field from default config.
